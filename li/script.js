b08d8b7fda82288e5661f6bdce84f7f3086c33f1
hyn10 <129762908+hyn10@users.noreply.github.com>
2023-04-03 21:53:00 +0800

    version1

---
+++ li/script.js
@@ -0,0 +1,23 @@
+let menu = document.querySelector("#menu-bar");
+let navbar = document.querySelector(".navbar");
+
+// 按下Toggle時的變化
+menu.onclick=()=>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+    if (window.scrollY>60){
+        document.querySelector('#scroll-top').classList.add('active');
+    }else{
+        document.querySelector('#scroll-top').classList.remove('active');
+    }
+}
+
+
+// scroll up 
+
